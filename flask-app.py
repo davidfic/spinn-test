3cc7fe083d1771d1592a83cf3a72cffe5ef346f3
David Ficociello <davidfic@gmail.com>
2017-12-09 09:40:40 -0500

    Update flask-app.py

--- flask-app.py
+++ flask-app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized and Spinnaker'
+    return 'Flask Dockerized and Spinnaker and testing'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
